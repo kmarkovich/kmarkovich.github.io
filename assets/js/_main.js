47a1de7883a6b068dbe1d2d33b2e2f3202df9956
Katie Markovich <kmarkovich@mac.mynetworksettings.com>
2025-06-16 16:53:12 -0600

    add two other pubs and revert toggle

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -19,11 +19,9 @@ $(document).ready(function () {
     if (use_theme === "dark") {
       $("html").attr("data-theme", "dark");
       $("#theme-icon").removeClass("fa-sun").addClass("fa-moon");
-      $("#theme-label").text("Dark Mode");
     } else if (use_theme === "light") {
       $("html").removeAttr("data-theme");
       $("#theme-icon").removeClass("fa-moon").addClass("fa-sun");
-      $("#theme-label").text("Light Mode");
     }
   };
 
